f370b8ecae8d6dd2e9b877dedd9c2b210ed9fa10
Jordana Kicköfel <jordana.grutzmann@gmail.com>
2025-08-25 16:18:03 -0300

    Atualiza profile.json

--- portfolio-js/assets/js/api.js
+++ portfolio-js/assets/js/api.js
@@ -1,7 +1,10 @@
 
 async function fetchProfileData() {
-    //sobe o projeto, vai data e clica em raw e daí copia o link e cola aqui
-    const url = '';
+    //Sobe o projeto para o Github;
+    //Vai em data/profile.json e clica em raw (topo direito);
+    //Vai aparecer o arquivo de json no navegador; 
+    //Copia esse link e cola aqui.
+    const url = 'https://raw.githubusercontent.com/JKickofel/JKickofel.github.io/refs/heads/main/portfolio-js/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
